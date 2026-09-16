4bb56f64608d3d0315068c8ece1bce3983676cd6
hfarcaya <hfarcaya@stefanini.com>
2026-09-16 13:53:43 -0300

    criando fluxo de cadastro

---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,17 @@
+describe("Cadastro de Usuário", () => {
+  it("deve cadastrar um novo usuário com sucesso", () => {
+    cy.visit("https://adopet-frontend-cypress.vercel.app");
+    
+    cy.contains("a", "Cadastrar").click();
+
+    const email = `joao${Math.floor(Math.random() * 10000)}@gmail.com`;
+
+    // preencher o formulário
+    cy.get('[data-test="input-name"]').type("João");
+    cy.get('[data-test="input-email"]').type(email);
+    cy.get('[data-test="input-password"]').type("Joao2006@2026");
+    cy.get('[data-test="input-confirm-password"]').type("Joao2006@2026");
+
+    cy.get('[data-test="submit-button"]').click();
+  });
+});
